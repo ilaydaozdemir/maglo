50e330354f0647cae97703cc37db32bc44ce3a78
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-10-02 12:34:49 +0300

    add input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,3 +1,29 @@
-export default function Input() {
-  return <div>inputs</div>;
-}
+import React from 'react';
+import styles from './Input.module.css';
+
+const Input = ({ 
+  type = 'text', 
+  placeholder = '', 
+  value = '', 
+  onChange = () => {},
+  name = '',
+  label = '',
+  ...props 
+}) => {
+  return (
+    <div className={styles.inputWrapper}>
+      {label && <label className={styles.label}>{label}</label>}
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        name={name}
+        className={styles.input}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default Input;
